ca315b4b655a2125a8e3ee57430a444909b8d32b
tanliping <tanliping@travelsky.com>
2018-06-27 17:30:58 +0800

    link组件

---
+++ modules/NavLink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class NavLink extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <Link {...this.props} activeClassName="active"/>
+        );
+    }
+}
