a0c2df083902b7af7a56a4d83059d3574ec60029
tanliping <tanliping@travelsky.com>
2018-06-27 16:52:19 +0800

    链接激活状态

--- modules/App.js
+++ modules/App.js
@@ -14,8 +14,8 @@ export default class App extends React.Component {
             <div>
                 <h1>React Router Tutorial</h1>
                 <ul role="nav">
-                    <li><Link to="/about" activeStyle={{color:'red'}}>About</Link></li>
-                    <li><Link to="/repos" activeStyle={{color:'red'}}>Repos</Link></li>
+                    <li><Link to="/about" activeClassName="active">About</Link></li>
+                    <li><Link to="/repos" activeClassName="active">Repos</Link></li>
                 </ul>
                 <div>{this.props.children}</div>
             </div>
