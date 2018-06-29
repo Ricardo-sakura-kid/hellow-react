f6c07d7510a78993cc8fefb84dd1b3ec22cb784f
tanliping <tanliping@travelsky.com>
2018-06-29 10:55:21 +0800

    IndexLink 组件

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute} from 'react-router';
 import App from './modules/App';
 //引入About和Repos组件
 import About from './modules/About';
-import Repos from './modules/Repos';
+import Repos from './modules/Repos'
+import Home from './modules/Home';
 
 //增加两个路由
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
+
+            <IndexRoute component={Home}/>
+
             <Route path="/about" component={About}/>
             <Route path="/repos" component={Repos}>
                 <Route path="/repos/:userName/:repoName" component={Repos}/>
--- modules/App.js
+++ modules/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 //导入Link组件
 //import { Link } from 'react-router';
 import NavLink from './NavLink';
+import Home from "./Home";
+import { IndexLink } from 'react-router';
 
 //定义App组件
 export default class App extends React.Component {
@@ -15,6 +17,11 @@ export default class App extends React.Component {
             <div>
                 <h1>React Router Tutorial</h1>
                 <ul role="nav">
+                    {/*<li><NavLink to="/">Home</NavLink></li>*/}
+
+                    {/* add here*/}
+                    <li><NavLink to="/" onlyActiveOnIndex={true}>Home</NavLink></li>
+
                     <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/repos">Repos</NavLink></li>
                 </ul>
